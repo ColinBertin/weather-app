e9d5c8f7c04d65dd14bb652a98079c6e15a75046
Colin Bertin <48044238+ColinBertin@users.noreply.github.com>
2023-02-10 14:56:26 +0900

    change spinner type

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -4,7 +4,13 @@ import React from "react";
 export default function LoadingSpinner() {
   return (
     <div className="grid justify-center content-center h-full">
-      <div className="loading-spinner"></div>
+      {/* <div className="loading-spinner"></div> */}
+      <div className="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
