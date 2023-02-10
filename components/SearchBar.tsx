07241ca01661a69f553e785b33dd0c1c46b0b603
Colin Bertin <48044238+ColinBertin@users.noreply.github.com>
2023-02-10 20:43:41 +0900

    add width to btn

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -12,18 +12,18 @@ export default function SearchBar({
   isLoading,
 }: SearchBarProps) {
   return (
-    <form className="flex justify-center content-around gap-2">
+    <form className="flex flex-col md:flex-row justify-center items-center content-around gap-2">
       <input
         type="text"
         id="first_name"
         onChange={(e) => handleChange(e)}
-        className="border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5"
+        className="border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 w-3/4"
       />
       <button
         type="submit"
         onClick={(e) => getCoords(e)}
         disabled={isLoading}
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="max-w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Search
       </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    maxWidth: {
+      '1/2': '50%',
+    },
     extend: {},
   },
   plugins: [],
