06b13b684116e4e1f3ae2118d64887c49bfea8ac
Colin Bertin <48044238+ColinBertin@users.noreply.github.com>
2023-02-14 12:56:41 +0900

    add humidity

--- components/PrevisionCards.tsx
+++ components/PrevisionCards.tsx
@@ -11,12 +11,15 @@ import {
   TiWeatherWindyCloudy,
   TiWeatherWindy,
 } from "react-icons/ti";
+import { WiHumidity } from "react-icons/wi";
 
 type PrevisionCardsProps = any;
 
 export default function PrevisionCards({ data }: PrevisionCardsProps) {
   const [date, setDate] = useState(0);
   const [month, setMonth] = useState(0);
+  const [sunrise, setSunrise] = useState(0);
+  const [sunset, setSunset] = useState(0);
 
   const handleIcon = (condition: string) => {
     switch (condition) {
@@ -37,22 +40,25 @@ export default function PrevisionCards({ data }: PrevisionCardsProps) {
     }
   };
 
+  console.log(data);
+
   useMemo(() => {
     const formattedDate = new Date(data.dt * 1000);
     setDate(formattedDate.getDate());
     setMonth(formattedDate.getMonth() + 1);
-  }, []);
+  }, [setDate, setMonth]);
 
   return (
     <div>
       <div className="flex flex-col justify-around items-center mt-4 w-24 h-36 border border-gray-300 rounded-lg">
         <div className="text-5xl">{handleIcon(data.weather[0].main)}</div>
-        <div className="text-sm">
-          <p>
+        <div className="">
+          <p className="mb-2">
             {date}/{month}
           </p>
-          <p>Min: {Math.round(data.temp.min)}°</p>
-          <p>Max: {Math.round(data.temp.max)}°</p>
+          <p className="flex justify-between">
+            {data.humidity} <WiHumidity className="text-2xl" />
+          </p>
         </div>
       </div>
     </div>
