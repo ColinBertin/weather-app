c750c2ca5a447879465faf0c673a3ad93eb75947
ColinBertin <colinbertin12@gmail.com>
2024-10-18 20:39:23 +0900

    add compass spinner

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -3,12 +3,7 @@ import React from "react";
 export default function LoadingSpinner() {
   return (
     <div className="grid justify-center content-center h-full">
-      <div className="lds-ellipsis">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+      <span className="loader"></span>
     </div>
   );
 }
