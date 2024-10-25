1fcb9a859e276ec1fa4d3abce529a70b10e21d43
ColinBertin <colinbertin12@gmail.com>
2024-10-25 23:18:21 +0900

    fix search bar & btn width

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -18,7 +18,7 @@ export default function SearchBar({
         id="first_name"
         placeholder="London"
         onChange={(e) => handleChange(e)}
-        className="border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 w-3/4"
+        className="border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 w-80"
       />
       <button
         type="submit"
@@ -27,7 +27,7 @@ export default function SearchBar({
           getCoords(e);
         }}
         disabled={isLoading}
-        className="max-w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="w-40 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Search
       </button>
